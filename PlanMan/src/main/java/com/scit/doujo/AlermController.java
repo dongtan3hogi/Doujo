b63889def5e4ea0c45ed1cbf72b6f747895502be
dongtan <sdujohn@gmail.com>
2018-09-11 20:22:04 +0900

    0911 Alerm03
    TESTController에 있던 메소드들 AlermController로 이동.

--- PlanMan/src/main/java/com/scit/doujo/AlermController.java
+++ PlanMan/src/main/java/com/scit/doujo/AlermController.java
@@ -159,4 +159,50 @@ public class AlermController {
 		
 		
 	}
+	
+	
+	
+	/* alerm에서 수락 ===================================================================*/ 
+	@RequestMapping(value = "/friendAlermOkBtn", method = RequestMethod.POST) 
+	public @ResponseBody Map<String, Map<String,String>> friendAlermOkBtn(@RequestBody Map<String, String> alerm, HttpSession hs) {
+		alermDao adao = sqlSession.getMapper(alermDao.class);
+		friendDao sdao = sqlSession.getMapper(friendDao.class);
+		String memberID = (String)hs.getAttribute("memberID");
+		Map<String, Map<String,String>> alermMap = new HashMap<>();
+		System.out.println("friendAlermOkBtn/alerm/"+alerm.toString());
+		//DB에 친구등록
+		int result1 = 0;
+		
+		
+		
+		
+		
+		
+		//해당 알람을 지운다.
+		int result2 = adao.deleteAlerm(alerm);
+		Map<String,String> AlermResult = new HashMap<>();
+		AlermResult.put("result1", ""+(result1+result2));
+		AlermResult.put("result2", "groupAlermOk");
+		alermMap.put("alermCount", AlermResult);
+		return alermMap; 
+	}
+	
+	
+	
+	
+	/* alerm에서 거절해서 해당 alerm을 테이블에서 지운다. ===================================================================*/ 
+	@RequestMapping(value = "/friendAlermNoBtn", method = RequestMethod.POST) 
+	public @ResponseBody Map<String, Map<String,String>> friendAlermNoBtn(@RequestBody Map<String, String> alerm, HttpSession hs) {
+		alermDao adao = sqlSession.getMapper(alermDao.class);
+		studyDao sdao = sqlSession.getMapper(studyDao.class);
+		Map<String, Map<String,String>> alermMap = new HashMap<>();
+		
+		//해당 알람을 지운다.
+		int result1 = adao.deleteAlerm(alerm);
+		Map<String,String> AlermResult = new HashMap<>();
+		AlermResult.put("result1", ""+result1);
+		AlermResult.put("result2", "friendAlermNoBtn");
+		alermMap.put("alermCount", AlermResult);
+		return alermMap; 
+	}
 } 
